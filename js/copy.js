a99360fdb0ae2193cf7ea87d87e7423e90a376dc
nurik99 <40524631+nurik99@users.noreply.github.com>
2019-01-26 12:47:54 +0600

    copy.js

---
+++ js/copy.js
@@ -0,0 +1,19 @@
+function wpguruLink() {
+    var istS = 'Источник:'; // Слово должно находится в кавычках!
+    var copyR = 'Nurik99.githab.io'; // Слово должно находится в кавычках!
+    var body_element = document.getElementsByTagName('body')[0];
+    var choose = window.getSelection();
+    var myLink = document.location.href;
+    var authorLink = "<br /><br />" + istS + ' ' + "<a href='"+myLink+"'>"+myLink+"</a><br />" + copyR;
+    var copytext = choose + authorLink;
+    var addDiv = document.createElement('div');
+    addDiv.style.position='absolute';
+    addDiv.style.left='-99999px';
+    body_element.appendChild(addDiv);
+    addDiv.innerHTML = copytext;
+    choose.selectAllChildren(addDiv);
+    window.setTimeout(function() {
+        body_element.removeChild(addDiv);
+    },0);
+}
+document.oncopy = wpguruLink;
